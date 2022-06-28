948f1bb4eecd815db32763f281c4e57345b390c6
Igor <filinigor@yandex.ru>
2022-06-28 20:03:11 +0300

    Object

---
+++ src/indexTraning.js
@@ -0,0 +1,35 @@
+export function Traning () {
+
+}
+
+const obj = {
+    name:'Igor',
+    age: 28
+}
+const obj2 = obj
+obj2.age = 29
+
+
+
+const copyObj = {...obj}
+console.log(obj === copyObj)//  false
+
+const copyObj2 = Object.assign(obj)
+
+console.log(copyObj2 === obj)// true
+
+let propsKey = 'Russia'
+
+let copyCopyUser = {...obj,[propsKey]:'My Country'}
+console.log(copyCopyUser)
+
+let newObj = {
+    name:'Igor',
+    age: 28,
+    status: 'online',
+    technologies: ['JS','HTML','CSS','ReactJS','TypeScript']
+}
+
+let copyNewObj = {...newObj,technologies:[...newObj.technologies]}
+console.log(copyNewObj === newObj) // false
+console.log(newObj.technologies === copyNewObj.technologies) // false
