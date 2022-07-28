b72a731b0fe2614fe644d6ba873f4e6b7c1ad19b
Igor <filinigor@yandex.ru>
2022-07-28 23:16:29 +0300

    tuesday-2-week.independent work

--- src/Monday-3-week/main.js
+++ src/Monday-3-week/main.js
@@ -1,1 +1,43 @@
 
+const students = [
+    {
+        name: "Bob",
+        age: 22,
+        isMarried: true,
+        scores: 85
+    },
+    {
+        name: "Alex",
+        age: 21,
+        isMarried: true,
+        scores: 90,
+    },
+    {
+        name: "Nick",
+        age: 20,
+        isMarried: false,
+        scores: 120
+    },
+    {
+        name: "John",
+        age: 19,
+        isMarried: false,
+        scores: 100
+    },
+    {
+        name: "Helen",
+        age: 20,
+        isMarried: false,
+        scores: 110
+    },
+    {
+        name: "Ann",
+        age: 20,
+        isMarried: false,
+        scores: 105
+    },
+];
+
+let deepCopyStudents = students.map(el => ({...el}))
+let bestStudent = deepCopyStudents.reduce((a, b) => a > b.scores ? a : b.scores,0)
+console.log(bestStudent)
